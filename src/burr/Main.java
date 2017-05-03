26c33e5d55ad70cb4b27fe9ce85799ce06073be8
Matthew Burr <mdburr@outlook.com>
2017-05-02 20:27:53 -0500

    Initial upload.

--- src/burr/Main.java
+++ src/burr/Main.java
@@ -1,8 +1,11 @@
 package burr;
 
+import com.google.gson.Gson;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    // write your code here
+    Gson g = new Gson();
+  }
 }
